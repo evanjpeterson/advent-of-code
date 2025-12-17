6ef8f8be22995ee96327198e065a0b03c2f946b4
Evan Peterson <evan@evanjp.com>
2025-12-17 14:36:17 +0100

    day09part1 simple

---
+++ 2025/days/09/part1.ts
@@ -0,0 +1,37 @@
+import { getInput, log, parseNumber } from "@/common"
+
+/**
+ * Advent of Code 2025 Day 8 Part 1
+ * https://adventofcode.com/2025/day/8
+ */
+
+type Pos = {
+  x: number
+  y: number
+}
+
+const calcArea = (p1: Pos, p2: Pos) =>
+  // We're going to count the rest of the 'tile' beyond the far point for the area calculation.
+  // It wouldn't surprise me if there's a proper mathematical term for this.
+  //
+  // OPTIMIZATION: points are sorted, so p2 is always >= p1. No need for Math.abs()
+  // (Math.abs(p1.x - p2.x) + 1) * (Math.abs(p1.y - p2.y) + 1)
+  (p2.x - p1.x + 1) * (p2.y - p1.y + 1)
+
+// Sort points by x, then y.
+const points = (await getInput())
+  .map((line) => {
+    const [x, y] = line.split(",").map(parseNumber)
+    return { x: x!, y: y! }
+  })
+  .sort((p1, p2) => p1.x - p2.x || p1.y - p2.y)
+
+let maxArea = 0
+for (let i = 0; i < points.length; i++) {
+  for (let j = i + 1; j < points.length; j++) {
+    const area = calcArea(points[i]!, points[j]!)
+    maxArea = area > maxArea ? area : maxArea
+  }
+}
+
+log(`Max area: ${maxArea}`)
